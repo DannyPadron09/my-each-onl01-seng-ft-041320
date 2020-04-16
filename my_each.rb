d393da8ec57087f37e8f2c9b9bd76bfec0eefea5
Danny Padron <danny.padron09@gmail.com>
2020-04-16 15:36:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
-def my_each(array)
+def my_each(array)#Declare method
+  #Set initial iteration value
   i = 0
+  #Set condition for loop
   while i < array.length
+    #Will yield the specific iteration to a seperate block
     yield(array[i])
+    #Increments up by one
     i += 1
   end
+  #Returns the array
   array
 end
